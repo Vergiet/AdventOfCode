54d1955f208309c970e7b3cd4c0ebc033d95dde8
Vergiet <djhhoornweg@hotmail.com>
2025-12-06 14:39:02 +0100

    fix day 5 part 2

--- 2025/day 5/solution.py
+++ 2025/day 5/solution.py
@@ -50,99 +50,17 @@ def evaluate_freshness(fresh_ranges, available_ingredients):
     return available_fresh_ingredients
 
 
-# def filter_ranges(fresh_ranges):
-#     filtered_ranges = []
-#     for start, end in fresh_ranges:
-#         for filtered_start, filtered_end in filtered_ranges:
-#             if start <= filtered_start and end > filtered_start and end <= filtered_end:
-#                 pass
-#             else:
-#                 filtered_ranges.append((start, end))
-#     return fresh_ranges
-
-
 def filter_ranges(fresh_ranges):
+    sorted_ranges = sorted(fresh_ranges, key=lambda x: x[0])
     filtered_ranges = []
-    for start, end in fresh_ranges:
-        add_to_list = True
-        print("Checking Filtered")
-        print(f"{start} - start")
-        print(f"{end} - end")
-        print("")
-        if len(filtered_ranges) == 0:
+    for start, end in sorted_ranges:
+        if not filtered_ranges or start > filtered_ranges[-1][1] + 1:
             filtered_ranges.append((start, end))
         else:
-            for filtered_start, filtered_end in filtered_ranges:
-                print("Checking Filtered")
-                print(f"{filtered_start} - start")
-                print(f"{filtered_end} - end")
-                print("")
-                # print()
-                if start == filtered_start and end == filtered_end:
-                    # print(f"{start} - start")
-                    # print(f"{filtered_start} - filtered_start")
-                    # print(f"{end} - end")
-                    # print(f"{filtered_end} - filtered_end")
-                    add_to_list = False
-                    # break
-                elif (
-                    start < filtered_start
-                    and end > filtered_start
-                    and end <= filtered_end
-                ):
-                    print("1")
-                    print(f"{start} - start")
-                    print(f"{filtered_start} - filtered_start")
-                    print(f"{end} - end")
-                    print(f"{filtered_end} - filtered_end")
-                    new_start = start
-                    new_end = filtered_end
-                    print(f"{new_start} - new_start")
-                    print(f"{new_end} - new_end")
-                    filtered_ranges.remove((filtered_start, filtered_end))
-                    print(f"adding to list {new_start} {new_end}")
-                    filtered_ranges.append((new_start, new_end))
-                    new_start = 0
-                    new_end = 0
-                    add_to_list = False
-                    # break
-                elif start >= filtered_start and end <= filtered_end:
-                    # print(f"{start} - start")
-                    # print(f"{filtered_start} - filtered_start")
-                    # print(f"{end} - end")
-                    # print(f"{filtered_end} - filtered_end")
-                    add_to_list = False
-                elif (
-                    start == filtered_start
-                    and start < filtered_end
-                    and end > filtered_end
-                ):
-                    print("2")
-                    print(f"{start} - start")
-                    print(f"{filtered_start} - filtered_start")
-                    print(f"{end} - end")
-                    print(f"{filtered_end} - filtered_end")
-                    new_start = start
-                    new_end = end
-                    print(f"{new_start} - new_start")
-                    print(f"{new_end} - new_end")
-                    filtered_ranges.remove((filtered_start, filtered_end))
-                    print(f"adding to list {new_start} {new_end}")
-                    filtered_ranges.append((new_start, new_end))
-                    new_start = 0
-                    new_end = 0
-                    add_to_list = False
-                    # break
-
-        print(f"add_to_list: {add_to_list}")
-        if add_to_list:
-            print(f"adding to list {start} {end}")
-            filtered_ranges.append((start, end))
-
-        print("Status:")
-        print(f"{len(fresh_ranges)} - fresh_ranges")
-        print(f"{len(filtered_ranges)} - filtered_ranges")
-        print("")
+            filtered_ranges[-1] = (
+                filtered_ranges[-1][0],
+                max(filtered_ranges[-1][1], end),
+            )
     return filtered_ranges
 
 
@@ -179,24 +97,12 @@ def main1(inputfile, expected_result_1=0, expected_result_2=0):
 def main2(inputfile, expected_result_1=0, expected_result_2=0):
     fresh_ranges, available_ingredients = parse_input(read_file(inputfile))
     filtered_ranges = filter_ranges(fresh_ranges)
-    print(f"Number of fresh ranges: {len(fresh_ranges)}")
-    print(f"Number of filtered ranges: {len(filtered_ranges)}")
     actual_result = calculate_fresh_ids(filtered_ranges)
     # actual_result = calculate_fresh_ids(fresh_ranges)
     # actual_result = 0
 
     parse_result(actual_result, expected_result_2, inputfile)
 
-
-# 49577959599068 to low
-# 49807208925952 to low
-# 61419725698598 to low
-# 58240463842684 to low
-# 355672034204528 not correct
-# 376909854969867 not correct
-# 427981489340584 not correct
-# 445171204094492 to high
-# 445171204094492 to high
 
 if __name__ == "__main__":
     for inputfile in (
