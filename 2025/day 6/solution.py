d271b96dd1349c7e46d429da741982d962830a2d
Vergiet <djhhoornweg@hotmail.com>
2025-12-06 11:29:38 +0100

    fix d6p2

--- 2025/day 6/solution.py
+++ 2025/day 6/solution.py
@@ -1,5 +1,4 @@
 import os
-import string
 
 
 def read_file(file):
@@ -60,58 +59,6 @@ def print_line(input):
     return answer
 
 
-# def print_line_2(input):
-#     list_of_list_of_vals = []
-#     for line in input.splitlines():
-#         print(line)
-
-#         new_val = []
-#         for char_index in range(0, len(line), 5):
-#             if char_index % 5:
-#                 pass
-#             else:
-#                 new_val.append(line[char_index : char_index + 4])
-#             # print(line[char_index : char_index + 4])
-
-#         list_of_list_of_vals.append(new_val)
-
-#         # list_of_list_of_vals.append(vals_on_line)
-
-#     answer = 0
-#     math_problems = []
-#     for index_in_row in range(len(list_of_list_of_vals[0])):
-#         math_problem = []
-#         for row in list_of_list_of_vals:
-#             math_problem.append(row[index_in_row])
-#         math_problems.append(math_problem)
-
-#     for problem in math_problems:
-#         values = []
-#         horizontal_values = []
-#         for char_index in range(3):
-#             horizontal_value_chars = []
-#             for value_index in range(len(problem)):
-#                 if problem[-1] != problem[value_index]:
-#                     value = problem[value_index]
-#                     char = value[char_index]
-#                     horizontal_value_chars.append(char)
-
-#                 # values.append(int("".join(problem[value_index])))
-#             horizontal_values.append(int("".join(horizontal_value_chars)))
-
-#         operator = problem[-1][0]
-#         if operator == "+":
-#             temp = 0
-#         else:
-#             temp = 1
-
-#         for value in horizontal_values:
-#             temp = apply_operator(operator, temp, value)
-#         answer = answer + temp
-
-#     return answer
-
-
 def print_line_2(input):
     operators = []
     for line in input.splitlines():
@@ -131,15 +78,20 @@ def print_line_2(input):
                     else:
                         operator = char
                         length += 1
+            operators.append((operator, 0))
 
     list_of_list_of_vals = []
     for line in input.splitlines():
         new_val = []
         previous_index = 0
         for operator in operators:
-            new_val.append(line[previous_index : (previous_index + operator[1])])
+            from_index = previous_index
+            if operator[1] == 0:
+                to_index = len(line)
+            else:
+                to_index = previous_index + operator[1]
+            new_val.append(line[from_index:to_index])
             previous_index += operator[1]
-            # print()
         list_of_list_of_vals.append(new_val)
 
     answer = 0
@@ -149,11 +101,10 @@ def print_line_2(input):
         for row in list_of_list_of_vals:
             math_problem.append(row[index_in_row])
         math_problems.append(math_problem)
-        # print()
 
     for problem in math_problems:
         horizontal_values = []
-        for char_index in range(3):  # fix this
+        for char_index in range(len(problem[0])):
             horizontal_value_chars = []
             for value_index in range(len(problem)):
                 if problem[-1] != problem[value_index]:
@@ -161,11 +112,10 @@ def print_line_2(input):
                     char = value[char_index]
                     horizontal_value_chars.append(char)
 
-                # values.append(int("".join(problem[value_index])))
             value = "".join(horizontal_value_chars)
             if len(value.strip()) > 0:
                 horizontal_values.append(int(value))
-        print()
+        # print()
         operator = problem[-1][0]
         if operator == "+":
             temp = 0
@@ -207,12 +157,10 @@ def main2(inputfile, expected_result_1=0, expected_result_2=0):
     parse_result(actual_result, expected_result_2, inputfile)
 
 
-# 11159825220288 to low
-
 if __name__ == "__main__":
     for inputfile in (
         # ("example", 4277556, 3263827),
-        ("input", 5877594983578, -1),
+        ("input", 5877594983578, 11159825706149),
     ):
         main1(*inputfile)
         main2(*inputfile)
