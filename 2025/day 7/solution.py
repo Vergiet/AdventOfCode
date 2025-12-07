f4c1cd2168d8bebef17bbb1fbbc3d68bb2be8dcf
Daniel Hoornweg <djhhoornweg@hotmail.com>
2025-12-07 09:09:45 +0100

    d7p2 met gesloten ideen

--- 2025/day 7/solution.py
+++ 2025/day 7/solution.py
@@ -58,7 +58,13 @@ def go_until_next_splitter(x, y, map, split_coords):
         # print()
         # print("Start new")
         # print_map(map)
-        asd = ""
+
+
+def create_timeline(timelines, split_coords):
+    new_index = len(timelines)
+    timelines.append(split_coords)
+    print(new_index)
+    return new_index
 
 
 def find_s(map):
@@ -69,11 +75,42 @@ def add_coords(split_coords, x, y):
     split_coords[f"{x},{y}"] = True
 
 
+def dan_maar_zo(x, y, map):
+    TimelinesperCell = []
+
+    for row in map:
+        TimelinesPerRow = []
+        for column in row:
+            TimelinesPerRow.append(0)
+        TimelinesperCell.append(TimelinesPerRow)
+    TimelinesperCell[x][y] = 1
+    for r in range(len(map) - 1):
+        for c in range(len(map[r])):
+            PossibleTimelines = TimelinesperCell[r][c]
+            if PossibleTimelines == 0:
+                continue
+            onedown = map[r + 1][c]
+            if onedown == "^":
+                rc = c + 1
+                if rc < len(map[r]):
+                    TimelinesperCell[r + 1][rc] += PossibleTimelines
+                rc = c - 1
+                if rc >= 0:
+                    TimelinesperCell[r + 1][rc] += PossibleTimelines
+            elif onedown == ".":
+                TimelinesperCell[r + 1][c] += PossibleTimelines
+    answer = 0
+    for c in range(len(map[0])):
+        answer += TimelinesperCell[len(map) - 2][c]
+    return answer
+
+
 def main1(inputfile, expected_result_1=0, expected_result_2=0):
     map = parse_input(read_file(inputfile))
     x, y = find_s(map)
 
     split_coords = dict()
+
     go_until_next_splitter(x, y, map, split_coords)
     actual_result = len(split_coords)
 
@@ -81,15 +118,17 @@ def main1(inputfile, expected_result_1=0, expected_result_2=0):
 
 
 def main2(inputfile, expected_result_1=0, expected_result_2=0):
-    actual_result = 0
+    map = parse_input(read_file(inputfile))
+    x, y = find_s(map)
+    actual_result = dan_maar_zo(x, y, map)
 
     parse_result(actual_result, expected_result_2, inputfile)
 
 
 if __name__ == "__main__":
     for inputfile in (
-        ("example", 21, -1),
-        ("input", -1, -1),
+        ("example", 21, 40),
+        ("input", 1651, 108924003331749),
     ):
         main1(*inputfile)
-        # main2(*inputfile)
+        main2(*inputfile)
